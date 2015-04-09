1b345e2e8ffae3a5f5ba3ac141e7b5149708fe8c
Dan Mordechay <danfromisrael@gmail.com>
2015-04-09 20:13:42 +0300

    Added config file for Environmental Variables and Config Vars should learn how to use them. URL: https://realpython.com/blog/python/flask-by-example-part-1-project-setup/

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn runserver:flask_runner
+web: gunicorn -b 127.0.0.1:8000 runserver:flask_runner
--- runserver.py
+++ runserver.py
@@ -1,15 +1,21 @@
 from server.AppStarter import AppStarter
 import os
 
+print('starting server... ToDo is coming right at ya!')
+
 static_folder_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "client")
 app = AppStarter()
 app.register_routes_to_resources(static_folder_root)
 flask_runner = app.get_flask_runner()  # flask app instance for gunicorn
-
 
 def runserver():
     app.run()
 
 if __name__ == '__main__':
     runserver()
+
+    # I should learn how to use those local environment variables...
+    # Heroku uses the settings from the .env file and i should define it someway in pycharm ide...
+    # secret_key = str(os.environ.get('APP_SETTINGS'))
+    # print('starting environment variables! - app settings are:' + secret_key)
 
