40d9042b288cfe2df958493374e2ec1435c175af
Dan Mordechay <danfromisrael@gmail.com>
2015-04-27 18:47:49 +0300

    integrated satellizer login into main app still only one global Todo list

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,127 +1,80 @@
-var scotchTodo = angular.module('scotchTodo', []);
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStrap', 'satellizer'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'js/partials/home.html',
+        controller: 'mainController'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'js/satellizer/partials/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'js/satellizer/partials/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .state('logout', {
+        url: '/logout',
+        template: null,
+        controller: 'LogoutCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'js/satellizer/partials/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          authenticated: function($q, $location, $auth) {
+            var deferred = $q.defer();
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+            if (!$auth.isAuthenticated()) {
+              $location.path('/login');
+            } else {
+              deferred.resolve();
+            }
 
-    // when landing on the page, get all todos and show them
-    $http.get('/api/todos')
-        .success(function(data) {
-            $scope.todos = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+            return deferred.promise;
+          }
+        }
+      });
 
-    // when submitting the add form, send the text to the node API
-    $scope.createTodo = function() {
-        $http.post('/api/todos', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    $urlRouterProvider.otherwise('/');
 
-    var removeFromArray = function(array,id){
-        var elementPos = array.map(function(x) {return x.id; }).indexOf(id)
-        array.splice(elementPos, 1);
-    }
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/todos/' + id)
-            .success(function(data) {
-                removeFromArray($scope.todos, id);
-                console.log($scope.todos);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    $authProvider.facebook({
+      clientId: '1579943425610440'
+    });
 
-}
+    $authProvider.google({
+      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+    });
 
-// ========================================================= //
-//
-//angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStrap', 'satellizer'])
-//  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
-//    $stateProvider
-//      .state('home', {
-//        url: '/',
-//        templateUrl: 'partials/home.html'
-//      })
-//      .state('login', {
-//        url: '/login',
-//        templateUrl: 'partials/login.html',
-//        controller: 'LoginCtrl'
-//      })
-//      .state('signup', {
-//        url: '/signup',
-//        templateUrl: 'partials/signup.html',
-//        controller: 'SignupCtrl'
-//      })
-//      .state('logout', {
-//        url: '/logout',
-//        template: null,
-//        controller: 'LogoutCtrl'
-//      })
-//      .state('profile', {
-//        url: '/profile',
-//        templateUrl: 'partials/profile.html',
-//        controller: 'ProfileCtrl',
-//        resolve: {
-//          authenticated: function($q, $location, $auth) {
-//            var deferred = $q.defer();
-//
-//            if (!$auth.isAuthenticated()) {
-//              $location.path('/login');
-//            } else {
-//              deferred.resolve();
-//            }
-//
-//            return deferred.promise;
-//          }
-//        }
-//      });
-//
-//    $urlRouterProvider.otherwise('/');
-//
-//    $authProvider.facebook({
-//      clientId: '603122136500203'
-//    });
-//
-//    $authProvider.google({
-//      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
-//    });
-//
-//    $authProvider.github({
-//      clientId: '45ab07066fb6a805ed74'
-//    });
-//
-//    $authProvider.linkedin({
-//      clientId: '77cw786yignpzj'
-//    });
-//
-//    $authProvider.yahoo({
-//      clientId: 'dj0yJmk9SDVkM2RhNWJSc2ZBJmQ9WVdrOWIzVlFRMWxzTXpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yYw--'
-//    });
-//
-//    $authProvider.twitter({
-//      url: '/auth/twitter'
-//    });
-//
-//    $authProvider.live({
-//      clientId: '000000004C12E68D'
-//    });
-//
-//    $authProvider.oauth2({
-//      name: 'foursquare',
-//      url: '/auth/foursquare',
-//      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
-//      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
-//      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
-//    });
-//  });
+    $authProvider.github({
+      clientId: '45ab07066fb6a805ed74'
+    });
+
+    $authProvider.linkedin({
+      clientId: '77cw786yignpzj'
+    });
+
+    $authProvider.yahoo({
+      clientId: 'dj0yJmk9SDVkM2RhNWJSc2ZBJmQ9WVdrOWIzVlFRMWxzTXpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yYw--'
+    });
+
+    $authProvider.twitter({
+      url: '/auth/twitter'
+    });
+
+    $authProvider.live({
+      clientId: '000000004C12E68D'
+    });
+
+    $authProvider.oauth2({
+      name: 'foursquare',
+      url: '/auth/foursquare',
+      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
+      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
+    });
+  });
---
+++ Client/js/controllers/myTodo.js
@@ -0,0 +1,87 @@
+angular.module('MyApp')
+  .controller('mainController', function($scope, $alert, $auth, $http) {
+  $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    var removeFromArray = function(array,id){
+        var elementPos = array.map(function(x) {return x.id; }).indexOf(id)
+        array.splice(elementPos, 1);
+    }
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                removeFromArray($scope.todos, id);
+                console.log($scope.todos);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+  });
+//
+//  function mainController($scope, $http) {
+//    $scope.formData = {};
+//
+//    // when landing on the page, get all todos and show them
+//    $http.get('/api/todos')
+//        .success(function(data) {
+//            $scope.todos = data;
+//            console.log(data);
+//        })
+//        .error(function(data) {
+//            console.log('Error: ' + data);
+//        });
+//
+//    // when submitting the add form, send the text to the node API
+//    $scope.createTodo = function() {
+//        $http.post('/api/todos', $scope.formData)
+//            .success(function(data) {
+//                $scope.formData = {}; // clear the form so our user is ready to enter another
+//                $scope.todos = data;
+//                console.log(data);
+//            })
+//            .error(function(data) {
+//                console.log('Error: ' + data);
+//            });
+//    };
+//
+//    var removeFromArray = function(array,id){
+//        var elementPos = array.map(function(x) {return x.id; }).indexOf(id)
+//        array.splice(elementPos, 1);
+//    }
+//    // delete a todo after checking it
+//    $scope.deleteTodo = function(id) {
+//        $http.delete('/api/todos/' + id)
+//            .success(function(data) {
+//                removeFromArray($scope.todos, id);
+//                console.log($scope.todos);
+//            })
+//            .error(function(data) {
+//                console.log('Error: ' + data);
+//            });
+//    };
+//
+//}
